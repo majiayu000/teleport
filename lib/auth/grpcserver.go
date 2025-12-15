8b410b2aa8ed6c4a9ce0b4de24b578bd473216f2
Marco Dinis <marco.dinis@goteleport.com>
2025-12-15 16:45:49 +0000

    remove unused struct field

--- lib/auth/grpcserver.go
+++ lib/auth/grpcserver.go
@@ -6275,7 +6275,6 @@ func NewGRPCServer(cfg GRPCServerConfig) (*GRPCServer, error) {
 		FIPS:               cfg.AuthServer.fips,
 		ScopedTokenService: cfg.AuthServer.Services,
 		OracleHTTPClient:   cfg.OracleHTTPClient,
-		AWSOrganizationsDescribeAccountClientGetter: cfg.AuthServer.GetAWSOrganizationsDescribeAccountClientGetter(),
 	}))
 
 	integrationServiceServer, err := integrationv1.NewService(&integrationv1.ServiceConfig{
